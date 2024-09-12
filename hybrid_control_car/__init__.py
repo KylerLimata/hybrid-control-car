359e8d8d14ff8685f293c0eec041bbb52789aea1
KylerLimata <kyler.limata@gmail.com>
2024-09-11 17:32:20 -0700

    Stub file
    Add typing hints for CarSimulation and its methods

---
+++ hybrid_control_car/__init__.py
@@ -0,0 +1,1 @@
+from .hybrid_control_car import *
---
+++ hybrid_control_car/hybrid_control_car.pyi
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class CarSimulation:
+    """
+    An instance of a simulated robot car and its environment.
+    """
+    def step(self, engine_force: float, steering_angle: float) -> List[float]:
+        """
+        Steps the simulation forward.
+        """
+    def reset_car(self):
+        """
+        Resets the simulated car.
+        """
